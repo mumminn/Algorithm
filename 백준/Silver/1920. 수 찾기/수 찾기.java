679ae24db77761a234ba7cd6644b2becd085f7d2
Kim haemin <117912034+mumminn@users.noreply.github.com>
2026-01-12 09:44:40 +0900

    [Silver IV] Title: 수 찾기, Time: 528 ms, Memory: 48476 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -6,23 +6,27 @@ class Main{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int n = Integer.parseInt(br.readLine());
-		Map <Integer, Integer> hashMap = new HashMap<>();
+		Set <Integer> set = new HashSet<>();
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i=0; i<n; i++) {
 			int a = Integer.parseInt(st.nextToken());
-			hashMap.put(a, hashMap.getOrDefault(a, 0) + 1);
+			set.add(a);
 		}
 		
 		int m = Integer.parseInt(br.readLine());
 		StringTokenizer str = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
 		for (int i=0; i<m; i++) {
 			int num = Integer.parseInt(str.nextToken());
-			if (hashMap.containsKey(num)) {
-				System.out.println(1);
+			if (set.contains(num)) {
+				sb.append(1 + "\n");
 			} else {
-				System.out.println(0);
+				sb.append(0 + "\n");
 			}
 		}	
+		
+		System.out.println(sb);
 	}
 }
