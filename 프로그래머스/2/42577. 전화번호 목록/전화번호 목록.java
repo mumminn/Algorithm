0eaee66a8d43bec74e946e314b64d71fbe276d75
Kim haemin <117912034+mumminn@users.noreply.github.com>
2026-01-14 09:22:13 +0900

    [level 2] Title: 전화번호 목록, Time: 332.61 ms, Memory: 98 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        
+        Arrays.sort(phone_book);
+        
+        for(int i=0; i<phone_book.length - 1; i++){
+            int len = phone_book[i].length();
+            
+            if(len < phone_book[i+1].length()){
+                 if(phone_book[i].equals(phone_book[i+1].substring(0, len))){
+                    answer = false;
+                    break;
+                }   
+            }
+        }
+        return answer;
+    }
+}
