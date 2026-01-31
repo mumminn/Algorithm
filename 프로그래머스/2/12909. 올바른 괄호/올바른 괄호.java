2eb5f5c08277a94037e38a9bf70498d457beb67e
Kim haemin <117912034+mumminn@users.noreply.github.com>
2026-01-31 17:28:37 +0900

    [level 2] Title: 올바른 괄호, Time: 18.44 ms, Memory: 55.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,35 +1,21 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-
         Stack<Character> stack = new Stack<>();
         
-        
-        for (int i = 0; i < s.length(); i++){
-            if (s.charAt(0) == ')'){
-                return(false);
-            }
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
             
-            if (s.charAt(i) == '('){
-                stack.push(s.charAt(i));
-            }  
-            if (s.charAt(i) == ')'){
+            if(ch == '('){
+                stack.push(ch);
+            } else {
                 if(stack.isEmpty()){
-                    return(false);
-                }
-                stack.pop();
+                    return false;
+                } else stack.pop();
             }
         }
         
-        if(stack.isEmpty()){
-            answer = true;
-        } else {
-            answer = false;
-        }
-        
-        return answer;
+        return stack.isEmpty();
     }
 }
